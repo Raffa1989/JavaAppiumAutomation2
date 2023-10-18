373e699b8ce33ba948231c5b327f03e2991fe550
Tatiana Koneva <con.t2012@yandex.ru>
2023-10-18 16:55:21 +0400

    тест

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,7 +3,13 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -34,8 +40,29 @@ public class FirstTest {
     }
 
     @Test
-    public void firstTest()
+    public void firstTest() {
+        WebElement element_to_init_search = driver.findElementByXPath("//*[contains(@text, 'Пропустить')]");
+        element_to_init_search.click();
+
+        //Thread.sleep(5000);
+
+        //WebElement element_to_init_search_1 = driver.findElementByXPath("//*[contains(@text, 'Поиск по Википедии')]");
+        //element_to_init_search_1.click();
+
+        WebElement element_to_enter_search_line = waitForElementPresentByXpath(
+                "//*[contains(@text, 'Поиск по Википедии')]",
+                "Cannot find search input",
+                5
+        );
+
+        element_to_enter_search_line.sendKeys("Языки программирования");
+    }
+
+    private WebElement waitForElementPresentByXpath (String xpath, String error_message, long timeoutInSeconds)
     {
-        System.out.println("First Test run");
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        By by = By.xpath(xpath);
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
     }
 }
